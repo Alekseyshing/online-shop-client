3b8f1d167008d26e736bd2701d9680a3c184eb29
Aleksey Shingarev <alekseytim@mail.ru>
2023-04-23 19:02:16 +0300

    feat: HeaderTop

---
+++ components/modules/Header/Header.tsx
@@ -0,0 +1,12 @@
+import styles from '@/styles/header/index.module.scss'
+import HeaderTop from './HeaderTop'
+
+const Header = () => {
+  console.log()
+  return (
+    <header className={styles.header}>
+      <HeaderTop />
+    </header>
+  )
+}
+export default Header
